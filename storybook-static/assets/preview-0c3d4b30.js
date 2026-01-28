baa5b7fed2cd19c95b00f9e6b3eee7686d8074ed
Ryu <ryu.forthepeople@gmail.com>
2026-01-28 08:55:34 +0100

    chore: add storybook-static to gitignore

--- storybook-static/assets/preview-0c3d4b30.js
+++
@@ -1,6 +0,0 @@
-const e={parameters:{backgrounds:{default:"dark",values:[{name:"dark",value:"#0a0e1f"},{name:"panel",value:"#11162d"},{name:"light",value:"#ffffff"}]},controls:{matchers:{color:/(background|color)$/i,date:/Date$/i}},designToken:{defaultTab:"Colors",styleInjection:'@import url("https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&family=Manrope:wght@400;500;600;700;800&display=swap");'}},decorators:[a=>({components:{story:a},template:`
-        <div style="font-family: 'Manrope', sans-serif; color: #ffffff;">
-          <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&family=Manrope:wght@400;500;600;700;800&display=swap" rel="stylesheet">
-          <story />
-        </div>
-      `})]};export{e as default};
